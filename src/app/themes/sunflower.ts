ab8debfa2452741d1d8f5543919b6cd94dddadff
Rudolf <rudolf@reindeer-rentals.com>
2021-07-10 13:11:53 +0200

    setting up

---
+++ src/app/themes/sunflower.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'sunflower',
+    templateUrl: './',
+    styleUrls: ['./sunflower.component.scss']
+  })
+  export class SunflowerComponent {
